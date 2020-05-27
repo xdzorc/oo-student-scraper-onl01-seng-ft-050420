d627908e45f3090b2334d22e54d3cf7b050ce34e
DAZUN XING <dazun.xing@gmail.com>
2020-05-27 00:42:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,9 +22,9 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, value|
-      if self.(#{attribute}) !=nil 
+      # if self.(#{attribute}) !=nil 
         self.send("#{attribute}=", value)
-      end
+      # end
     end
      self 
   end
